e6024334fdc59de6877815fa88895cb4c0f8060b
han0won <han00won@gmail.com>
2024-03-14 15:11:28 +0900

    first commit

---
+++ assets/js/comm.js
@@ -0,0 +1,51 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+    gsap.registerPlugin(ScrollTrigger);
+
+    gsap.to(".intro-box", {
+        scale: 1,
+        scrollTrigger: {
+          trigger: ".intro-box", 
+          start: "top center",  
+          end: "top center",  
+          scrub: 3,  
+          // markers: true,  
+        },
+    });
+
+    gsap.fromTo(".title", {
+        x:"100%",
+        }, 
+        {
+        x:"-150%",
+        scrollTrigger: {
+          trigger: ".full_screen", 
+          start: "top center",  
+          end: "bottom",  
+          scrub: 3,  
+        //   markers: true,  
+        },
+    });
+
+    let tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: ".bacchus-type",
+          pin: true, 
+          start: "top", 
+          end: "bottom", 
+          scrub: 1, 
+          snap: {
+            snapTo: "types", 
+            duration: 0.2, 
+            // delay: 0.2,
+            ease: "Linear",
+          },
+        },
+      });
+
+    tl.addLabel("types")
+    .from(".type2", { x:"100%"} )
+    .from(".type3", { y:"100%"} )
+    
+});
+
+
